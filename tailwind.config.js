57071c5c2093d389c7ae1669bb8279ac4d06efd0
Aaron-Ford <thekneun@gmail.com>
2021-03-24 21:21:59 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,13 @@ module.exports = {
     extend: {
       colors: {},
     },
+    fontFamily: {
+      'sans': ['"Spartan"'],
+      'serif': ['"Noto Serif"'],
+      'mono': ['"Noto Serif"'],
+      'display': ['"Spartan"'],
+      'body': ['"Noto Serif"'],
+    }    
   },
   variants: {},
   plugins: [require("@tailwindcss/typography")],
