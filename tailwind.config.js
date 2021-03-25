7a8f0ae740fd41c857c79cef0ad5860179b10b7d
Aaron-Ford <thekneun@gmail.com>
2021-03-24 20:13:08 -0600

    fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,13 @@ module.exports = {
     extend: {
       colors: {},
     },
+    fontFamily: {
+      'sans': ['Spartan', 'ui-sans-serif', 'system-ui', ...],
+      'serif': ['Noto Serif', 'ui-serif', 'Georgia', ...],
+      'mono': ['ui-monospace', 'SFMono-Regular', ...],
+      'display': ['Oswald', ...],
+      'body': ['Open Sans', ...],
+    },
   },
   variants: {},
   plugins: [require("@tailwindcss/typography")],
